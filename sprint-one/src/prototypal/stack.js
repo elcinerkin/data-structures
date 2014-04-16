d59e45eb7cb7320cc5d51f37ccdb9e5ae26ce9d9
Elcin <elcinerkin@gmail.com>
2014-04-16 16:20:03 -0700

    completed prototypal stack.js

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,39 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.storageSize = 0; // Hint: set an initial value here
+
+  return instance;
 };
 
 var stackMethods = {};
+
+// Implement the methods below
+stackMethods.push = function(value){
+  // push value into the storage object
+  this.storage[this.storageSize] = value;
+  // increment size
+  this.storageSize++;
+};
+
+stackMethods.pop = function(){
+  // decrement size
+  if(this.storageSize) {
+    this.storageSize--;
+  }
+  // cache last item
+  var item = this.storage[this.storageSize];
+  // delete last item
+  delete this.storage[this.storageSize];
+  // return cached last item
+  return item;
+};
+
+stackMethods.size = function(){
+  // returns size
+  return this.storageSize;
+};

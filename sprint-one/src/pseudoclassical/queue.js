ec833c7e1909edb6eff24cc405ccfe43f5605532
Elcin <elcinerkin@gmail.com>
2014-04-16 16:42:49 -0700

    finished implementing pseudoclassical stack AND queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,36 @@
 var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  //declare endOfQueue variable
+  this.endOfQueue = 0;
+  //declare front of the queue item
+  this.frontOfQueue = 0;
 };
+
+Queue.prototype.enqueue = function(value){
+    //set storage[size] to value
+    this.storage[this.endOfQueue] = value;
+    this.endOfQueue++;
+  };
+
+Queue.prototype.dequeue = function(){
+  if(this.endOfQueue !== this.frontOfQueue) {
+    //find first item
+    //cache first item
+    var item = this.storage[this.frontOfQueue];
+    //delete first item
+    delete this.storage[this.frontOfQueue];
+    //increment the frontOfQueue index
+    this.frontOfQueue++;
+    //return cached item
+    return item;
+  }
+};
+
+Queue.prototype.size = function(){
+  //return size
+  return this.endOfQueue - this.frontOfQueue;
+};
+
+var queue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,38 @@
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.storageSize = 0; // Hint: set an initial value here
+
 };
+
+Stack.prototype.stackMethods = {};
+
+// Implement the methods below
+Stack.prototype.push = function(value){
+  // push value into the storage object
+  this.storage[this.storageSize] = value;
+  // increment size
+  this.storageSize++;
+};
+
+Stack.prototype.pop = function(){
+  // decrement size
+  if(this.storageSize) {
+    this.storageSize--;
+  }
+  // cache last item
+  var item = this.storage[this.storageSize];
+  // delete last item
+  delete this.storage[this.storageSize];
+  // return cached last item
+  return item;
+};
+
+Stack.prototype.size = function(){
+  // returns size
+  return this.storageSize;
+};
+
+var stack = new Stack();

99fadb9eceaffd183445e6a259cc04133db5ec28
Elcin <elcinerkin@gmail.com>
2014-04-16 14:46:32 -0700

    started queue implementation (passing first 3 specs)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,22 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  //declare size variable
+  var size =0;
   // Implement the methods below
 
   instance.enqueue = function(value){
+    //set storage[size] to value
+    storage[size] = value;
+    size++;
   };
 
   instance.dequeue = function(){
   };
 
   instance.size = function(){
+    //return size
+    return size;
   };
 
   return instance;

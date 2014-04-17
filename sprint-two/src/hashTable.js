432acd462903a248180f904d6d774e40e94bc6d9
Elcin <elcinerkin@gmail.com>
2014-04-17 16:38:52 -0700

    started and finished hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,13 +4,21 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  //get the hash for input
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //store value at hash
+  this._storage.set(i, v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //return value at hash
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
+  //remove value at hash
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, null);
 };
 

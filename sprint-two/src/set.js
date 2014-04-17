f9b211019cdd95974f85b77e1ae66092f0e83c91
Elcin <elcinerkin@gmail.com>
2014-04-17 16:08:58 -0700

    started and finished set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -6,11 +6,20 @@ var makeSet = function(){
 
 var setPrototype = {};
 
-setPrototype.add = function(item){
+setPrototype.add = function(item) {
+  // check storage is defined
+  // if not, create storage as an object
+  this._storage = this._storage || {};
+  // add item to storage
+  this._storage[item] = true;
 };
 
-setPrototype.contains = function(item){
+setPrototype.contains = function(item) {
+  // check if storage contains property
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+  // remove item from storage
+  delete this._storage[item];
 };

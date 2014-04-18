3a29168d5c570a02296e905ce02dd10a28f37b0a
Elcin <elcinerkin@gmail.com>
2014-04-17 22:43:17 -0700

    switched order on insert conditional

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -14,16 +14,16 @@ BinarySearchTree.prototype.insert = function(newValue){
   console.dir(newBST);
 
   if(newValue < this.value) {             // check if bst's value is less than the current node's value.
-    if(this.left === null) {              // check if there is a left.
-      this.left = newBST;                 // if not, set bst as left
-    } else {                              // if there is a left,
+    if(this.left) {              // check if there is a left.
       this.left.insert(newValue);         // recurse on left.
+    } else {                              // if there is a left,
+      this.left = newBST;                 // if not, set bst as left
     }
   } else if(newValue > this.value) {      // can we refactor these 2 into 1 block?
-    if(this.right === null) {
-      this.right = newBST;
-    } else {
+    if(this.right) {
       this.right.insert(newValue);
+    } else {
+      this.right = newBST;
     }
   }
 };

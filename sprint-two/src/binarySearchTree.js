d6f40a158a41457ae09bcd1eb47935180007e190
Elcin <elcinerkin@gmail.com>
2014-04-17 22:22:00 -0700

    accomplished contains (its pretty clean)

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -13,15 +13,13 @@ BinarySearchTree.prototype.insert = function(newValue){
   var newBST = new BinarySearchTree(newValue);  // instantiate new bst for newItem
   console.dir(newBST);
 
-  // debugger;
-
-  if(newValue < this.value) {    // check if bst's value is less than the current node's value.
-    if(this.left === null) {             // check if there is a left.
-      this.left = newBST;                   // if not, set bst as left
-    } else {                        // if there is a left,
-      this.left.insert(newValue);      // recurse on left.
+  if(newValue < this.value) {             // check if bst's value is less than the current node's value.
+    if(this.left === null) {              // check if there is a left.
+      this.left = newBST;                 // if not, set bst as left
+    } else {                              // if there is a left,
+      this.left.insert(newValue);         // recurse on left.
     }
-  } else if(newValue > this.value) {
+  } else if(newValue > this.value) {      // can we refactor these 2 into 1 block?
     if(this.right === null) {
       this.right = newBST;
     } else {
@@ -31,8 +29,30 @@ BinarySearchTree.prototype.insert = function(newValue){
 };
 // create contains method
 BinarySearchTree.prototype.contains = function(target) {
-
+  // compare current item with target value
+  // if equal, return true
+  if(target === this.value) {
+    return true;
+  // if less and there is a left, recurse target against left
+  } else if (target < this.value && this.left !== null) {
+    return this.left.contains(target);
+  // if greater and there is a right, recurse target against right
+  } else if (target > this.value && this.right !== null) {
+    return this.right.contains(target);
+  // else return false;
+  } else {
+    return false;
+  }
 };
 // create depthFirstLog method
 BinarySearchTree.prototype.depthFirstLog = function() {};
+
+
+
+
+
+
+
+
+
 

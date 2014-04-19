56972041d0ba2efdb3697184785f2b82edc32e16
Elcin <elcinerkin@gmail.com>
2014-04-18 22:01:47 -0700

    wrote extensions for doublyLinked, 90% finished removeTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,7 +1,12 @@
-var makeLinkedList = function(){
+var makeLinkedList = function(type){
+
+
   var list = {};
+  list.type = type || 1;
   list.head = null;
   list.tail = null;
+    //do an extend to doubly linked list methods
+  _.extend(list, extraMethods);
 
   var newNode;
 
@@ -11,6 +16,9 @@ var makeLinkedList = function(){
     // if it's not the first item assign tail.next to newNode's value
     if(list.tail) {
       list.tail.next = newNode;
+      if(this.type === 2) {
+        newNode.previous = list.tail;
+      }
     }
     else {
       list.head = newNode;
@@ -48,10 +56,62 @@ var makeLinkedList = function(){
   return list;
 };
 
-var makeNode = function(value){
+var makeNode = function(value, type){
+  type = type || 1;
+
   var node = {};
   node.value = value;
   node.next = null;
 
+  if(type === 2) {
+    _.extend(node, doublyLinkedNode);
+  }
+
   return node;
 };
+
+// make doubly linked list methods and properties in an object
+var doublyLinkedNode = {};
+
+doublyLinkedNode.previous = null;
+
+var extraMethods = {};
+
+extraMethods.removeTail = function(node) {
+  var end;
+  // if type is singly linked list
+  if (this.type === 1) {
+    node = node || this.head;
+  // iterate through singly linked list to find second to last node
+    if (node.next && node.next.next) {
+      end = this.removeTail(node.next);
+    }
+    else if(node.next && !node.next.next) {
+      this.tail = node;
+      end = node.next;
+      node.next = null;
+    }
+    else if (!node.next) {        // 1-item linked list
+      end = node;
+      this.head = null;
+      this.tail = null;
+      return end;
+    } else {                      // 0-item linked list
+      end = null;
+    }
+    return end;                   // TODO: make it return value, not node
+  } else if(this.type === 2) {
+
+  }
+};
+
+extraMethods.addToHead = function() {};
+
+
+
+
+
+
+
+
+
